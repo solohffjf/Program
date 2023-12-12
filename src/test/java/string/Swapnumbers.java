2e1e5e052a633e0d597c2b884a97fd9950c1e928
Uva K.P <mukeshraam96@gmail.com>
2023-12-12 18:36:12 +0530

    second commit

--- src/test/java/string/Swapnumbers.java
+++ src/test/java/string/Swapnumbers.java
@@ -7,14 +7,17 @@ public class Swapnumbers {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int x,y,temp;
-		Scanner sc = new Scanner(System.in);
-		
-		x=sc.nextInt();
-		y=sc.nextInt();
-		
-		
-
-	}
+		int x, y, temp;
+        System.out.println("Enter x and y");
+        Scanner in = new Scanner(System.in);
+        x = in.nextInt();
+        y = in.nextInt();
+        System.out.println("Before Swapping" + x + y);
+        temp = x;
+        x = y;
+        y = temp;
+        System.out.println("After Swapping" + x + y);
+         
+    }
 
 }
